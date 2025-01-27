dd5eb2a60ba1a5f15f217a673f00e671b349d585
BrianLeishman <BrianLeishman@users.noreply.github.com>
2025-01-27 16:22:57 +0000

    deploy: c9bdac6f10ab0ce584adde152a602c19c8d9a044

---
+++ js/app.17069b936f9374b755757130c682c3d3c5bc50e1ed8de469705db7dfe33f5e4c.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var src, dst;
+
+var err;
+
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.23.0/min/vs' }});
+require(['vs/editor/editor.main'], function() {
+    monaco.editor.defineTheme('error', {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [
+            { token: 'custom-error', foreground: 'ff0000' },
+        ]
+    });
+
+    src = monaco.editor.create(document.getElementById('src'), {
+        value: '',
+        language: 'text',
+        theme: 'error',
+        minimap: {
+            enabled: false
+        }
+    });
+    src.onDidFocusEditorText(() => setTimeout(() => src.setSelection(src.getModel().getFullModelRange()), 100));
+
+    // Register a new language
+    monaco.languages.register({ id: 'error' });
+
+    // Register a tokens provider for the language
+    monaco.languages.setMonarchTokensProvider('error', {
+        tokenizer: {
+            root: [
+                [/.*/, "custom-error"],
+            ]
+        }
+    });
+
+    dst = monaco.editor.create(document.getElementById('dst'), {
+        value: '',
+        language: 'json',
+        theme: 'error',
+        minimap: {
+            enabled: false
+        },
+    });
+    dst.onDidFocusEditorText(() => setTimeout(() => dst.setSelection(dst.getModel().getFullModelRange()), 100));
+
+    let settingSrc = false;
+    let settingDst = false;
+
+    src.getModel().onDidChangeContent(() => {
+        if (settingSrc) return;
+        settingDst = true;
+        monaco.editor.setModelLanguage(src.getModel(), "text");
+        dst.getModel().setValue(JSON.stringify(src.getModel().getValue()));
+        settingDst = false;
+    });
+
+    dst.getModel().onDidChangeContent(() => {
+        if (settingDst) return;
+        settingSrc = true;
+        try {
+            monaco.editor.setModelLanguage(src.getModel(), "text");
+            src.getModel().setValue(JSON.parse(dst.getModel().getValue()));
+        } catch (e) {
+            monaco.editor.setModelLanguage(src.getModel(), "error")
+            src.getModel().setValue(err);
+        } finally {
+            settingSrc = false;
+        }
+    });
+});
